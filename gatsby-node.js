d2742303321f8cdbeb3bae26f98f67f55247f76f
Reza Rahman <rezrah@github.com>
2022-04-07 18:03:38 +0100

    linters fml

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,9 +3,7 @@ const defines = require('./babel-defines')
 exports.onCreateWebpackConfig = ({actions, plugins, getConfig}) => {
   const config = getConfig()
   // Add our `__DEV__` and `process.env.NODE_ENV` defines
-  config.plugins.push(
-    plugins.define(defines[process.env.NODE_ENV || 'development']),
-  )
+  config.plugins.push(plugins.define(defines[process.env.NODE_ENV || 'development']))
 
   // Polyfill `path` and stub `fs` for use in the browser
   // https://www.gatsbyjs.com/docs/reference/release-notes/migrating-from-v2-to-v3/#webpack-5-node-configuration-changed-nodefs-nodepath-
@@ -13,12 +11,12 @@ exports.onCreateWebpackConfig = ({actions, plugins, getConfig}) => {
     ...config.resolve,
     alias: {
       ...config.resolve.alias,
-      path: require.resolve('path-browserify'),
+      path: require.resolve('path-browserify')
     },
     fallback: {
       ...config.resolve.fallback,
-      fs: false,
-    },
+      fs: false
+    }
   }
 
   actions.replaceWebpackConfig(config)
